dc8d3f0b11a9fd09aaccfcd8b3bb20d49e7e4556
Edison Mora <edison.mora@gorillalogic.com>
2020-07-10 01:29:27 -0600

    create details page

--- src/pages/details/index.js
+++ src/pages/details/index.js
@@ -1,7 +1,35 @@
 import React, { useEffect, useState } from "react";
-import { Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Box,
+  Chip,
+  Paper,
+  Typography,
+  List,
+  ListItem,
+} from "@material-ui/core";
 
 import services from "../../services";
+import utils from "../../utils";
+
+const useStyles = makeStyles((theme) => ({
+  stat: {
+    fontWeight: "600",
+    marginRight: theme.spacing(3),
+  },
+  statsItem: {
+    justifyContent: "space-between",
+  },
+  abilityItem: {
+    flexDirection: "column",
+  },
+  abilityName: {
+    fontWeight: "600",
+  },
+  chip: {
+    marginRight: theme.spacing(1),
+  },
+}));
 
 async function loadPokemonData({ setPokemon, pokemonId }) {
   const fullPokemonData = await services.getPokemonById(pokemonId);
@@ -9,16 +37,86 @@ async function loadPokemonData({ setPokemon, pokemonId }) {
 }
 
 function Details(props) {
+  const classes = useStyles();
   const {
     match: { params },
   } = props;
-  const [pokemon, setPokemon] = useState({});
+  const [pokemon, setPokemon] = useState(null);
 
   useEffect(() => {
     loadPokemonData({ setPokemon, pokemonId: params.id });
   }, [params]);
 
-  return <Box>{pokemon.name}</Box>;
+  return (
+    <Box padding={2}>
+      <Paper>
+        {pokemon ? (
+          <Box
+            padding={2}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+          >
+            <Typography variant="h4">
+              {utils.capitalize(pokemon.name)}
+            </Typography>
+            <Box>
+              <img src={pokemon.sprites.front_default} />
+              <img src={pokemon.sprites.back_default} />
+            </Box>
+            <Box>
+              {pokemon.types.map((item) => (
+                <Chip
+                  key={item.slot}
+                  className={classes.chip}
+                  label={item.type.name}
+                />
+              ))}
+            </Box>
+            <Typography variant="h5">Base Stats</Typography>
+            <List>
+              <ListItem className={classes.statsItem}>
+                <Typography className={classes.stat} variant="body1">
+                  Weight:
+                </Typography>
+                <Typography variant="body2">{pokemon.weight}</Typography>
+              </ListItem>
+              {pokemon.stats.map((item) => (
+                <ListItem
+                  key={item.base_stat + item.stat.name}
+                  className={classes.statsItem}
+                >
+                  <Typography className={classes.stat} variant="body1">
+                    {utils.capitalize(item.stat.name) + ":"}
+                  </Typography>
+                  <Typography variant="body2">{item.base_stat}</Typography>
+                </ListItem>
+              ))}
+            </List>
+            <Typography variant="h5">Abilities</Typography>
+            <List>
+              {pokemon.abilities.map((item) => (
+                <ListItem
+                  key={item.ability.name}
+                  className={classes.abilityItem}
+                >
+                  {item.is_hidden && (
+                    <Typography variant="caption">Hidden ability</Typography>
+                  )}
+                  <Typography className={classes.abilityName} variant="body1">
+                    {utils.capitalize(item.ability.name)}
+                  </Typography>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        ) : (
+          <Typography>Loading...</Typography>
+        )}
+      </Paper>
+    </Box>
+  );
 }
 
 export default Details;
